f525f6c1a79492eef2d0f2ab2bab621edd738fc2
wooyoung <andy241822@gmail.com>
2024-02-22 01:03:20 +0900

    [UPDATE] props에 sell 추가

--- src/AuctionList/AuctionListTable.js
+++ src/AuctionList/AuctionListTable.js
@@ -1,56 +1,61 @@
 import React, {useEffect, useState} from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import {DataGrid} from '@mui/x-data-grid';
 import AuctionListJoinButton from "./AuctionListJoinButton";
 import axios from "../Config/AxiosConfig";
 import {useNavigate} from "react-router-dom";
+
 const renderJoinCell = (params) => {
     return <div>{params.value}</div>;
 };
 const columns = [
-    { field: 'image', headerName: '물품', flex:1,sortable:false, renderCell: renderJoinCell },
-    { field: 'name', headerName: '물품명', flex:2,sortable:false  },
-    { field: 'seller', headerName: '판매자', flex:1,sortable:false  },
-    { field: 'sellDate', headerName: '경매시작일시', flex:2,sortable:false  },
-    { field: 'startPrice', headerName: '시작가격', flex:1,sortable:false  },
-    { field: 'nowPrice', headerName: '현재가격', flex:1,sortable:false  },
-    { field: 'join', headerName: '참가하기', flex:1,sortable:false,renderCell: renderJoinCell,  },
+    {field: 'image', headerName: '물품', flex: 1, sortable: false, renderCell: renderJoinCell},
+    {field: 'name', headerName: '물품명', flex: 2, sortable: false},
+    {field: 'seller', headerName: '판매자', flex: 1, sortable: false},
+    {field: 'sellDate', headerName: '경매시작일시', flex: 2, sortable: false},
+    {field: 'startPrice', headerName: '시작가격', flex: 1, sortable: false},
+    {field: 'nowPrice', headerName: '현재가격', flex: 1, sortable: false},
+    {field: 'join', headerName: '참가하기', flex: 1, sortable: false, renderCell: renderJoinCell,},
 ];
 
-function AuctionListTable(props) {
+function AuctionListTable({sell}) {
     const navigate = useNavigate()
-    const navigateAuctionPage = (no)=>{
+    const navigateAuctionPage = (no) => {
         navigate(`/list/${no}`)
     }
     const [rows, setRows] = useState([])
-    useEffect( ()=>{
-        setRows([]);
-        async function getListData() {
-           const res = await axios.get('/getListData')
-            res.data.map(value=>{
-                const imageUrl = `data:image/jpeg;base64,${value.image}`;
-                const newRows = {
-                    id: value.no,
-                    image: <img className={"w50"} src={imageUrl} alt="Base64 Image" />,
-                    name: value.contentName,
-                    seller: value.seller,
-                    sellDate: value.startTime,
-                    startPrice: value.startPrice,
-                    nowPrice: value.nowPrice,
-                    join: <AuctionListJoinButton navigateAuctionPage={navigateAuctionPage} no={value.no}/>,
-                }
-                setRows(prevValue=>([...prevValue,newRows]))
-            })
+    useEffect(() => {
+        if (sell === false) {
+            setRows([]);
+
+            async function getListData() {
+                const res = await axios.get('/getListData')
+                res.data.map(value => {
+                    const imageUrl = `data:image/jpeg;base64,${value.image}`;
+                    const newRows = {
+                        id: value.no,
+                        image: <img className={"w50"} src={imageUrl} alt="Base64 Image"/>,
+                        name: value.contentName,
+                        seller: value.seller,
+                        sellDate: value.startTime,
+                        startPrice: value.startPrice,
+                        nowPrice: value.nowPrice,
+                        join: <AuctionListJoinButton navigateAuctionPage={navigateAuctionPage} no={value.no}/>,
+                    }
+                    setRows(prevValue => ([...prevValue, newRows]))
+                })
+            }
+
+            getListData();
         }
-        getListData();
-    },[])
+    }, [sell])
     return (
-        <div style={{ height: '98%', width: '100%' }}>
+        <div style={{height: '98%', width: '100%'}}>
             <DataGrid
                 rows={rows}
                 columns={columns}
                 initialState={{
                     pagination: {
-                        paginationModel: { page: 0, pageSize: 5 },
+                        paginationModel: {page: 0, pageSize: 5},
                     },
                 }}
                 sortingOrder={[]}
